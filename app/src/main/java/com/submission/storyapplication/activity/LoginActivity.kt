ed47bc29a856cba66bfde57b2efce0c7ac8ee84a
Yudha Yunanto Putra <yudha.yunanto.2010@gmail.com>
2022-09-14 15:53:35 +0700

    disable button sudah, custom password belum

--- app/src/main/java/com/submission/storyapplication/activity/LoginActivity.kt
+++ app/src/main/java/com/submission/storyapplication/activity/LoginActivity.kt
@@ -2,11 +2,26 @@ package com.submission.storyapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import com.submission.storyapplication.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setupView()
+    }
+
+    fun setupView(){
+        btn_login.setOnClickListener {
+            if(btn_login.text.toString().equals("Show")){
+                ed_login_password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            } else{
+                ed_login_password.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+        }
     }
 }
