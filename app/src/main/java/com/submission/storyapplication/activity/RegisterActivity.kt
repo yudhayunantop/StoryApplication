504ad6c177689e61db1d267158dafc4f0cad6a9b
Yudha Yunanto Putra <yudha.yunanto.2010@gmail.com>
2022-09-14 13:48:29 +0700

    tampilan fix, customView belum beres

---
+++ app/src/main/java/com/submission/storyapplication/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.submission.storyapplication.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.submission.storyapplication.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
