589c5bcdb7dac23cb52e002f67f733af2972888d
Yudha Yunanto Putra <yudha.yunanto.2010@gmail.com>
2022-09-20 13:07:02 +0700

    login beres, add story berhasil, tapi masih ngerotate

--- app/src/main/java/com/submission/storyapplication/activity/MainActivity.kt
+++ app/src/main/java/com/submission/storyapplication/activity/MainActivity.kt
@@ -3,19 +3,18 @@ package com.submission.storyapplication.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.submission.storyapplication.R
 import com.submission.storyapplication.adapter.StoriesAdapter
 import com.submission.storyapplication.api.ApiRetrofit
 import com.submission.storyapplication.models.AllStoriesModel
-import com.submission.storyapplication.models.LoginModel
 import com.submission.storyapplication.preferences.Preferences
+import com.submission.storyapplication.preferences.Preferences.clearData
 import com.submission.storyapplication.preferences.Preferences.getToken
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.Interceptor
-import okhttp3.Request
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -77,4 +76,21 @@ class MainActivity : AppCompatActivity() {
 
                 })
         }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                val i = Intent(this, LoginActivity::class.java)
+                clearData()
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
+    }
+}
