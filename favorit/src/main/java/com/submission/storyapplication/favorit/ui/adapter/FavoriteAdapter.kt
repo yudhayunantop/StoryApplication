4e3ea2ee7a50c87e97bd0c98f45a9d82ad3bf499
Yudha Yunanto Putra <yudha.yunanto.2010@gmail.com>
2022-12-02 10:02:06 +0700

    set gambar dan favorite bisa masuk ke detail

--- favorit/src/main/java/com/submission/storyapplication/favorit/ui/adapter/FavoriteAdapter.kt
+++ favorit/src/main/java/com/submission/storyapplication/favorit/ui/adapter/FavoriteAdapter.kt
@@ -1,10 +1,17 @@
 package com.submission.storyapplication.favorit.ui.adapter
 
+import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.submission.storyapplication.core.data.remote.response.AllStoriesModel
 import com.submission.storyapplication.core.databinding.ItemStoryFavoriteBinding
+import com.submission.storyapplication.ui.activity.DetailActivity
 
 class FavoriteAdapter: RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
     var listStories = listOf<AllStoriesModel.stories>()
@@ -33,10 +40,29 @@ class FavoriteAdapter: RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val current = listStories[position]
         with(holder){
+            Glide.with(itemView.context)
+                .load(current.photoUrl)
+                .apply(RequestOptions().override(55, 55))
+                .into(binding.ivItemPhoto)
             binding.tvItemName.text = current.name
             binding.tvItemDesc.text = current.description
             binding.btnDelete.setOnClickListener {
                 onClickListener.onButtonDeleteSelected(current)
+            }
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, DetailActivity::class.java)
+                intent.putExtra("data", current)
+
+                val optionsCompat: ActivityOptionsCompat =
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        itemView.context as Activity,
+                        Pair(binding.ivItemPhoto, "profile"),
+                        Pair(binding.tvItemName, "name"),
+                        Pair(binding.tvItemDesc, "description")
+                    )
+
+                itemView.context.startActivity(intent, optionsCompat.toBundle())
             }
         }
     }
