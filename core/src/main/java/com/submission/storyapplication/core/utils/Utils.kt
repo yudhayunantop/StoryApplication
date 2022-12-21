f90faac85484448ba0a76babb285667151713ecb
Yudha Yunanto Putra <yudha.yunanto.2010@gmail.com>
2022-12-21 15:09:44 +0700

    Progress submission 1. utils diupdate 2. favorite disesuaikan 3. add package leaksmemory 4. local.properties diupdate

--- core/src/main/java/com/submission/storyapplication/core/utils/Utils.kt
+++ core/src/main/java/com/submission/storyapplication/core/utils/Utils.kt
@@ -28,7 +28,7 @@ fun createCustomTempFile(context: Context): File {
 fun rotateBitmap(bitmap: Bitmap, isBackCamera: Boolean = false): Bitmap {
     val matrix = Matrix()
     return if (isBackCamera) {
-        matrix.postRotate(360f)
+        matrix.postRotate(90f)
         Bitmap.createBitmap(
             bitmap,
             0,
@@ -39,7 +39,7 @@ fun rotateBitmap(bitmap: Bitmap, isBackCamera: Boolean = false): Bitmap {
             true
         )
     } else {
-        matrix.postRotate(-90f)
+        matrix.postRotate(90f)
         matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
         Bitmap.createBitmap(
             bitmap,
